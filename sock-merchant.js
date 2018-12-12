7be069c5ab9692b1b116d109ef0ed0dd95c6ac5a
Monica Lee <lee.smonica@gmail.com>
2018-12-12 14:20:07 -0800

    Resolve failing test cases

--- sock-merchant.js
+++ sock-merchant.js
@@ -3,17 +3,13 @@ function sockMerchant(n, ar) {
     ar.sort((a, b) => { return a - b })
 
     let counter = 0;
-    while (ar.length > 0) {
-        if (ar[0] === ar[1]) {
-            counter++
-            ar.splice(0, 2)
-        } else {
-            ar.splice(0, 2)
+    for (let i = 0; i < n; i++) {
+        if (ar[i] === ar[i + 1]) {
+            counter++;
+            i++;
         }
     }
     return counter;
 }
 
 console.log(sockMerchant(9, [10, 20, 20, 10, 10, 30, 50, 10, 20]))
-
-// work-in-progress: 3/9 test cases failed
